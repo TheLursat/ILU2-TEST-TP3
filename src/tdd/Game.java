c03b6c400867a976da800bc3e357fd060dc1dd39
TheLursat <lucas_massat@msn.com>
2023-11-13 09:30:31 +0100

    test aucune quille

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -1,5 +1,23 @@
 package tdd;
 
 public class Game {
-
+	
+	private static final int NB_MAX_LANCES = 21;
+	private int[] tab = new int[NB_MAX_LANCES];
+	private int num_tour;
+	
+	public Game() {
+		num_tour=0;
+		for(int i=0; i<NB_MAX_LANCES;i++) {
+			tab[i]=0;
+		}
+	}
+	
+	void roll( int nbQuilles) {
+		
+	}
+	
+	int score() {
+		return 0;
+	}
 }
